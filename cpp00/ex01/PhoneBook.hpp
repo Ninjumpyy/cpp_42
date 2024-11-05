eb3ab33bdcbc4e3480508267960ee25780cae466
Thomas Le Moel <tle-moel@c3r3s2.42luxembourg.lu>
2024-11-05 17:03:41 +0100

    start ex01 cpp00

---
+++ cpp00/ex01/PhoneBook.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tle-moel <tle-moel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 16:43:42 by tle-moel          #+#    #+#             */
+/*   Updated: 2024/11/05 16:47:57 by tle-moel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+#define PHONENOOK_HPP
+
+class PhoneBook
+{
+	private:
+		// Private attribute, only accessible within the class
+
+	public:
+		//Constructor
+
+		// Getter method
+
+		// Setter method
+};
+
+#endif
