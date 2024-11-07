fd323fdf1f7aeec851ce7c29d7e1fda024af7993
Thomas <thomas@tle-moel.fritz.box>
2024-11-07 16:23:08 +0100

    little change on ex00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tle-moel <tle-moel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: thomas <thomas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 13:45:56 by tle-moel          #+#    #+#             */
-/*   Updated: 2024/11/05 15:58:10 by tle-moel         ###   ########.fr       */
+/*   Updated: 2024/11/07 15:37:42 by thomas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ int	main(int argc, char **argv)
 	}
 	else
 	{
-		std::string message = argv[1];
 		for (int i = 1; i < argc; i++)
 		{
 			std::string message = argv[i];
