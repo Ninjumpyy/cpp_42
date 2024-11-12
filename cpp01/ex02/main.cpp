657da472acce2bf5b10b284574d13b549ed935eb
Thomas Le Moel <tle-moel@c3r4s2.42luxembourg.lu>
2024-11-12 18:15:00 +0100

    ex02 cpp01 done

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,31 @@
 /*   By: tle-moel <tle-moel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 18:01:21 by tle-moel          #+#    #+#             */
-/*   Updated: 2024/11/12 18:02:48 by tle-moel         ###   ########.fr       */
+/*   Updated: 2024/11/12 18:13:42 by tle-moel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+int	main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+
+	std::cout << "The memory address of the string variable : "
+			  << &str << "\n";
+	std::cout << "The memory address held by stringPTR : "
+			  << stringPTR << "\n";
+	std::cout << "The memory address held by stringREF : "
+			  << &stringREF << "\n";
+	std::cout << std::endl;
+	std::cout << "Value of str : " << str << std::endl;
+	std::cout << "Value pointed to by stringPTR : "
+			  << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF : "
+			  << stringREF << std::endl;
+
+	return (0);
+}
