01ef8efa9077b4960700229517838da09814d62f
Thomas <thomas@tle-moel.fritz.box>
2024-11-14 10:43:42 +0100

    ex05 cpp01 done

--- cpp01/ex05/Harl.hpp
+++ cpp01/ex05/Harl.hpp
@@ -6,7 +6,25 @@
 /*   By: thomas <thomas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 18:12:41 by thomas            #+#    #+#             */
-/*   Updated: 2024/11/13 18:12:42 by thomas           ###   ########.fr       */
+/*   Updated: 2024/11/14 09:14:53 by thomas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <string>
+
+class Harl
+{
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+
+	public:
+		void	complain(std::string level);
+};
+
+#endif
--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -6,7 +6,23 @@
 /*   By: thomas <thomas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 18:12:44 by thomas            #+#    #+#             */
-/*   Updated: 2024/11/13 18:12:44 by thomas           ###   ########.fr       */
+/*   Updated: 2024/11/14 09:48:37 by thomas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+#include <string>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cerr << "Usage: ./Harl level\n";
+		return (1);
+	}
+	
+	Harl	harl;
+	harl.complain(argv[1]);
+	return (0);
+}
