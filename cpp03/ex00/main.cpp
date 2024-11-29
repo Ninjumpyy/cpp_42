ec34d43c6b1564ad9a8b02dbe188e45d4a574ca1
Thomas Le Moel <tle-moel@c3r6s4.42luxembourg.lu>
2024-11-29 10:52:20 +0100

    change in the tester before evaluation

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tle-moel <tle-moel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 16:14:55 by thomas            #+#    #+#             */
-/*   Updated: 2024/11/25 14:28:44 by tle-moel         ###   ########.fr       */
+/*   Updated: 2024/11/29 10:51:16 by tle-moel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,23 @@ int	main()
 {
     ClapTrap noname;
     ClapTrap clappy("Clappy");
-    ClapTrap claptrap("Billy");
-    ClapTrap billy(clappy);
-    billy = noname;
+    ClapTrap billy("Billy");
+    ClapTrap copy_billy(billy);
+    ClapTrap copy_clappy;
+    copy_clappy = clappy;
 
     noname.attack("Enemy 1");
     clappy.attack("Enemy 2");
-    billy.attack("Ennemy 3");
+    copy_billy.attack("Ennemy 3");
 
     clappy.takeDamage(3);
 	clappy.beRepaired(5); // Repair when energy points are available
 
-    clappy.takeDamage(15); // Test for more damage than current hit points
+    clappy.takeDamage(11); // Test for more damage than current hit points
     clappy.beRepaired(20); 
 
     // Deplete energy points to test behavior
-    for (int i = 0; i < 10; ++i)
+    for (int i = 0; i < 7; ++i)
         clappy.attack("Dummy");
 
     // Attempt to attack and repair when out of energy points
