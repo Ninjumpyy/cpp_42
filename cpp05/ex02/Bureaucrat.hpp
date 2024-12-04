0e693d81b11b64d53708feb2fe17cc447ff7a967
Thomas <thomas@tle-moel.fritz.box>
2024-12-04 11:31:22 +0100

    ex02 created

---
+++ cpp05/ex02/Bureaucrat.hpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thomas <thomas@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/02 12:44:37 by thomas            #+#    #+#             */
+/*   Updated: 2024/12/04 11:30:40 by thomas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <string>
+# include <exception>
+# include "AForm.hpp"
+
+class AForm;
+
+class Bureaucrat
+{
+	private:
+		std::string const _name;
+		int	_grade;
+		
+	public:
+		//Constructor
+		Bureaucrat();
+		Bureaucrat(std::string const &name, int grade);
+
+		//Copy Constructor
+		Bureaucrat(const Bureaucrat& other);
+
+		//Copy Assignment Operator
+		Bureaucrat& operator=(const Bureaucrat& other);
+
+		//Destructor
+		~Bureaucrat();
+
+		//Member functions
+		std::string const& getName() const;
+		int getGrade() const;
+		void incrementGrade();
+		void decrementGrade();
+
+		void signForm(AForm& f);
+
+		//Exceptions
+		class GradeTooHighException : public std::exception
+		{
+			const char *what() const throw();	
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			const char *what() const throw();
+		};
+};
+
+//Overload of operator
+std::ostream& operator<<(std::ostream &os, const Bureaucrat& other);
+
+#endif
