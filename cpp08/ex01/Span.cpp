7873fd69ab2830ccf2cca4d273bbb65e7a85a878
Thomas Le Moel <tle-moel@c3r6s5.42luxembourg.lu>
2024-12-16 17:03:44 +0100

    ex01 in process

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -3,11 +3,65 @@
 /*                                                        :::      ::::::::   */
 /*   Span.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thomas <thomas@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tle-moel <tle-moel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 16:05:50 by thomas            #+#    #+#             */
-/*   Updated: 2024/12/12 16:05:56 by thomas           ###   ########.fr       */
+/*   Updated: 2024/12/16 17:02:42 by tle-moel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
+
+//Constructors
+Span::Span(): 
+{
+	V = 
+}
+
+Span::Span(unsigned int n): N(n) {}
+
+Span::Span(const Span& other): N(other.N) {}
+
+//Copy Assignment Operator
+Span& Span::operator=(const Span& other)
+{
+	if (this != &other)
+	{
+		N = other.N;
+	}
+}
+
+//Destructor
+Span::~Span() {}
+
+//Member functions
+void	Span::addNumber(int nb)
+{
+	
+}
+
+unsigned int	Span::shortestSpan()
+{
+
+}
+
+unsigned int	Span::longestSpan()
+{
+	
+}
+
+void	Span::addRandomNumbers(unsigned int size)
+{
+	
+}
+
+//Exceptions
+const char* Span::TooManyElements::what() const throw()
+{
+	return "Span is full. It's not possible to add any more number\n";
+}
+
+const char* Span::TooFewElements::what() const throw()
+{
+	return "Not enough elements in the Span\n";
+}
--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -3,16 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   Span.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thomas <thomas@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tle-moel <tle-moel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 16:05:59 by thomas            #+#    #+#             */
-/*   Updated: 2024/12/12 16:06:15 by thomas           ###   ########.fr       */
+/*   Updated: 2024/12/16 17:02:40 by tle-moel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SPAN_HPP
 # define SPAN_HPP
 
+# include <exception>
+# include <vector>
 
+class Span
+{
+	private:
+		std::vector<int> V;
+		
+	public:
+		//Constructors
+		Span();
+		Span(unsigned int n);
+		Span(const Span& other);
+
+		//Copy Assignment Operator
+		Span& operator=(const Span& other);
+
+		//Destructor
+		~Span();
+
+		//Member functions
+		void	addNumber(int nb);
+		unsigned int shortestSpan();
+		unsigned int longestSpan();
+		void	addRandomNumbers(unsigned int size);
+
+		//Exceptions
+		class TooManyElements: public std::exception
+		{
+			const char *what() const throw();
+		};
+
+		class TooFewElements: public std::exception
+		{
+			const char *what() const throw();
+		};
+};
 
 #endif
