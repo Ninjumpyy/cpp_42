e3d0d3c94c8ce9257d444ecf28cf2d49bb8420bf
Thomas Le Moel <tle-moel@c3r5s4.42luxembourg.lu>
2025-01-29 11:08:49 +0100

    cpp09 finished

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: tle-moel <tle-moel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 18:11:40 by thomas            #+#    #+#             */
-/*   Updated: 2025/01/28 16:46:43 by tle-moel         ###   ########.fr       */
+/*   Updated: 2025/01/29 10:58:40 by tle-moel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,13 +54,13 @@ int PMergeMe::fordjohnson(char **argv)
 	start = clock();
 	std::vector<int> vec_sorted = fj_vector(vec, 1);
 	end = clock();
-	double vectortime = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+	double vectortime = 1000 * static_cast<double>(end - start) / CLOCKS_PER_SEC;
 
 	// Ford-Johnson with a deque container
 	start = clock();
 	std::deque<int> deq_sorted = fj_deque(deq, 1);
 	end = clock();
-	double dequetime = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+	double dequetime =  1000 * static_cast<double>(end - start) / CLOCKS_PER_SEC;
 
 	print_result(vec, vec_sorted, vectortime, dequetime);
 	return 0; //Indicate success...
@@ -81,10 +81,8 @@ std::vector<int> PMergeMe::fj_vector(std::vector<int> vec, int n)
 {
 	//Step1 : Division into pairs and sorting
 
-	if ((n * 2) > static_cast<int>(vec.size())) {
-		//std::cout << "END OF STEP 1 : n = " << n << std::endl << std::endl;
+	if ((n * 2) > static_cast<int>(vec.size()))
 		return vec;
-	}
 	
 	int nb_comp = static_cast<int>(vec.size()) / (n * 2);
 	int idx1 = n - 1;
@@ -92,24 +90,16 @@ std::vector<int> PMergeMe::fj_vector(std::vector<int> vec, int n)
 
 	for (int i = 0; i < nb_comp; i++)
 	{
-		//std::cout << "vec[idx2] : " << vec[idx2] << " and vec[idx1] : " << vec[idx1] << std::endl;
 		if (vec[idx2] < vec[idx1])
 		{
 			std::vector<int>::iterator range1 = vec.begin() + (2 * n * i);
-			//std::cout << "range1 : " << *range1 << std::endl;
 			std::vector<int>::iterator range2 = range1 + n;
-			//std::cout << "range2 : " << *range2 << std::endl;
 			std::swap_ranges(range1, range2, range2);
 		}
 
 		idx2 += (2 * n);
 		idx1 += (2 * n);
 	}
-	/*std::cout << "STEP 1 : n = " << n << std::endl;
-	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); it++) {
-		std::cout << *it << " ";
-	}
-	std::cout << std::endl;*/
 	vec = fj_vector(vec, n * 2);
 
 	//Step2 : binary insertion algo
@@ -121,27 +111,12 @@ std::vector<int> PMergeMe::fj_vector(std::vector<int> vec, int n)
 		to_insert.push_back(vec[i]);
 	}
 
-
-	/*std::cout << "STEP 2 : n = " << n << std::endl;
-	std::cout << "TO_INSERT : \n";
-	for (std::vector<int>::iterator it = to_insert.begin(); it != to_insert.end(); it++) {
-	std::cout << *it << " ";
-	}
-	std::cout << std::endl;*/
-
 	// Init search
 	std::vector<int> search;
 	search.push_back(vec[n - 1]);
 	for (int i = (n * 2) - 1; i < static_cast<int>(vec.size()); i += (2 * n)) {
 		search.push_back(vec[i]);
 	}
-
-	
-	/*std::cout << "SEARCH : \n";
-	for (std::vector<int>::iterator it = search.begin(); it != search.end(); it++) {
-	std::cout << *it << " ";
-	}
-	std::cout << std::endl;*/
 
 	//Init new_vec that will be my new vector
 	std::vector<int> new_vec;
@@ -150,41 +125,20 @@ std::vector<int> PMergeMe::fj_vector(std::vector<int> vec, int n)
 		std::copy (vec.begin() + i, vec.begin() + i + n, std::back_inserter(new_vec));
 	}
 
-	/*std::cout << "NEW VEC : \n";
-	for (std::vector<int>::iterator it = new_vec.begin(); it != new_vec.end(); it++) {
-	std::cout << *it << " ";
-	}
-	std::cout << std::endl << std::endl;*/
-
 	//Binary insertion
 	int k = 2;
-
-	//std::cout << "BINARY INSERTION\n";
-	//std::cout << "jacobsthal number when k = " << k << " is " << jacobsthal_number(k) << std::endl;
 
 	while (jacobsthal_number(k) <= static_cast<int>(to_insert.size()))
 	{
 		int i = jacobsthal_number(k);
 		while (i > jacobsthal_number(k-1))
 		{
-			std::vector<int>::iterator it = std::lower_bound(search.begin(), search.end(), to_insert[i - 1]);
+			std::vector<int>::iterator it = std::lower_bound(search.begin(), search.begin() + (pow(2, k) - 1), to_insert[i - 1]);
 			int index = it - search.begin();
 			int range = 2 * (i - 1) * n;
 
 			search.insert(search.begin() + index, to_insert[i - 1]);
 			new_vec.insert(new_vec.begin() + (index * n), vec.begin() + range, vec.begin() + range + n);
-
-			/*std::cout << "SEARCH AFTER b" << i << std::endl;
-			for (std::vector<int>::iterator it = search.begin(); it != search.end(); it++) {
-				std::cout << *it << " ";
-			}
-			std::cout << std::endl;
-
-			std::cout << "NEW_VEC AFTER b" << i << std::endl;
-			for (std::vector<int>::iterator it = new_vec.begin(); it != new_vec.end(); it++) {
-				std::cout << *it << " ";
-			}
-			std::cout << std::endl;*/
 
 			i--;
 		}
@@ -195,24 +149,11 @@ std::vector<int> PMergeMe::fj_vector(std::vector<int> vec, int n)
 	int i = to_insert.size();
 	while (i > jacobsthal_number(k-1))
 	{
-		//std::cout << "INSERTING LAST B's...\n";
 		std::vector<int>::iterator it = std::lower_bound(search.begin(), search.end(), to_insert[i - 1]);
 		int index = it - search.begin();
 		int range = 2 * (i - 1) * n;
 		search.insert(search.begin() + index, to_insert[i - 1]);
 		new_vec.insert(new_vec.begin() + (index * n), vec.begin() + range, vec.begin() + range + n);
-
-		/*std::cout << "SEARCH AFTER b" << i << std::endl;
-		for (std::vector<int>::iterator it = search.begin(); it != search.end(); it++) {
-			std::cout << *it << " ";
-		}
-		std::cout << std::endl;
-
-		std::cout << "NEW_VEC AFTER b" << i << std::endl;
-		for (std::vector<int>::iterator it = new_vec.begin(); it != new_vec.end(); it++) {
-			std::cout << *it << " ";
-		}
-		std::cout << std::endl;*/
 
 		i--;
 	}
@@ -220,17 +161,9 @@ std::vector<int> PMergeMe::fj_vector(std::vector<int> vec, int n)
 
 
 	int rem = static_cast<int>(vec.size()) % n;
-	if (rem != 0) {
-		//std::cout << "INSERTING REMINDER...\n";
+	if (rem != 0)
 		new_vec.insert(new_vec.end(), vec.begin() + static_cast<int>(vec.size()) - rem, vec.end());
-	}
 
-
-	/*std::cout << "STEP 2 : n = " << n << std::endl;
-	for (std::vector<int>::iterator it = new_vec.begin(); it != new_vec.end(); it++) {
-	std::cout << *it << " ";
-	}
-	std::cout << std::endl << std::endl;*/
 	return (new_vec);
 }
 
@@ -249,7 +182,7 @@ std::deque<int> PMergeMe::fj_deque(std::deque<int> deq, int n)
 	{
 		if (deq[idx2] < deq[idx1])
 		{
-			std::deque<int>::iterator range1 = deq.begin() + ( 2 * i * n);
+			std::deque<int>::iterator range1 = deq.begin() + (2 * n * i);
 			std::deque<int>::iterator range2 = range1 + n;
 			std::swap_ranges(range1, range2, range2);
 		}
@@ -260,13 +193,6 @@ std::deque<int> PMergeMe::fj_deque(std::deque<int> deq, int n)
 	deq = fj_deque(deq, n * 2);
 
 	//Step2 : binary insertion algo
-
-	if (nb_comp == 1)
-	{
-		if (deq[(n * 2) - 1] < deq[n - 1])
-			std::swap_ranges(deq.begin(), deq.begin() + n, deq.begin() + n);
-		return deq;
-	}
 
 	// Init to_insert
 	std::deque<int> to_insert;
@@ -285,40 +211,32 @@ std::deque<int> PMergeMe::fj_deque(std::deque<int> deq, int n)
 	//Init new_deq that will be my new deque
 	std::deque<int> new_deq;
 	new_deq.insert(new_deq.end(), deq.begin(), deq.begin() + (n * 2));
-	for (int i = (n * 3); i < static_cast<int>(deq.size()); i += (2 * n)) {
+	for (int i = (n * 3); (i + n - 1) < static_cast<int>(deq.size()); i += (2 * n)) {
 		std::copy (deq.begin() + i, deq.begin() + i + n, std::back_inserter(new_deq));
 	}
 
 	//Binary insertion
 	int k = 2;
-	while (jacobsthal_number(k) < static_cast<int>(to_insert.size()))
+
+	while (jacobsthal_number(k) <= static_cast<int>(to_insert.size()))
 	{
 		int i = jacobsthal_number(k);
 		while (i > jacobsthal_number(k-1))
 		{
-			std::deque<int>::iterator it = std::lower_bound(search.begin(), search.end(), to_insert[i - 1]);
+			std::deque<int>::iterator it = std::lower_bound(search.begin(), search.begin() + (pow(2, k) - 1), to_insert[i - 1]);
 			int index = it - search.begin();
 			int range = 2 * (i - 1) * n;
+
 			search.insert(search.begin() + index, to_insert[i - 1]);
 			new_deq.insert(new_deq.begin() + (index * n), deq.begin() + range, deq.begin() + range + n);
+
 			i--;
 		}
 		k++;
 	}
-	/*
-	for (k; jacobsthal_number(k) < to_insert.size(); k++) {
-		int i = jacobsthal_number(k);
-		while (i > jacobsthal_number(k-1))
-		{
-			std::vector<int>::iterator it = std::lower_bound(search.begin(), search.end(), to_insert[i - 1]);
-			int index = it - search.begin();
-			int range = 2 * (i - 1) * n;
-			search.insert(search.begin() + index, to_insert[i - 1]);
-			new_vec.insert(new_vec.begin() + (index * n), vec.begin() + range, vec.begin() + range + n);
-			i--;
-		}
-	}*/
-	int i = to_insert.size() - 1;
+
+
+	int i = to_insert.size();
 	while (i > jacobsthal_number(k-1))
 	{
 		std::deque<int>::iterator it = std::lower_bound(search.begin(), search.end(), to_insert[i - 1]);
@@ -326,8 +244,16 @@ std::deque<int> PMergeMe::fj_deque(std::deque<int> deq, int n)
 		int range = 2 * (i - 1) * n;
 		search.insert(search.begin() + index, to_insert[i - 1]);
 		new_deq.insert(new_deq.begin() + (index * n), deq.begin() + range, deq.begin() + range + n);
+
 		i--;
 	}
+
+
+
+	int rem = static_cast<int>(deq.size()) % n;
+	if (rem != 0)
+		new_deq.insert(new_deq.end(), deq.begin() + static_cast<int>(deq.size()) - rem, deq.end());
+
 	return (new_deq);
 }
 
@@ -350,6 +276,6 @@ void PMergeMe::print_result(std::vector<int> vec, std::vector<int> vec_sorted, d
 	}
 	std::cout << std::endl;
 
-	std::cout << "Time to process a range of " << vec.size() << " elements with std::vector : " << std::fixed << std::setprecision(6) << vectortime << " s\n";
-	std::cout << "Time to process a range of " << vec.size() << " elements with std::deque : " << std::fixed << std::setprecision(6) << dequetime << " s\n";
+	std::cout << "Time to process a range of " << vec.size() << " elements with std::vector : " << std::fixed << std::setprecision(6) << vectortime << " ms\n";
+	std::cout << "Time to process a range of " << vec.size() << " elements with std::deque : " << std::fixed << std::setprecision(6) << dequetime << " ms\n";
 }
